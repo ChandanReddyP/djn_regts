342fd72c80ce17ec61d3e8954fdde92a8ffb3f91
ChandanReddyP <chandanreddyp777@gmail.com>
2024-08-19 14:35:57 +0530

    first commit

---
+++ app/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from app.models import *
+
+
+class Userform(forms.ModelForm):
+    class Meta():
+        model=User
+        fields=['username','email','password']
+
+class Profileform(forms.ModelForm):
+    class Meta():
+        model=Profile
+        fields=['profile_pic','address']
--- app/views.py
+++ app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from app.forms import *
+from django.http import HttpResponse
+
+def registration(request):
+    EUFO=Userform()
+    EPFO=Profileform()
+    d={'EUFO':EUFO,'EPFO':EPFO}
+    
+    return render(request,'registration.html',d)
